b81b03f512ac08d0784d01c95b4e2a2cc48e46a6
huu1712 <huu10a10vts@gmail.com>
2022-10-12 17:10:55 +0700

    update

--- index.js
+++ index.js
@@ -16,50 +16,53 @@ createApp({
             this.choose.push(val)
             if (this.choose.length > 1) {
                 this.choose.splice(event, 1)
-            }console.log(this.choose)
+            }
         },
 
         randomIndex() {
-            var randomNumber = Math.floor(Math.random() * 3)
+            if(Object.keys(this.choose).length === 0){
+                alert('Bạn chưa chọn Kéo, Búa hoặc Bao')
+            }else {
+                var randomNumber = Math.floor(Math.random() * 3)
 
-            if (randomNumber == 0) {
-                randomNumber = 'Kéo'
-                this.computer = randomNumber
-            } else if (randomNumber == 1) {
-                randomNumber = 'Búa'
-                this.computer = randomNumber
-            } else {
-                randomNumber = 'Bao'
-                this.computer = randomNumber
-            }
-
-            if (this.choose == randomNumber) {
-                this.result = 'Bạn HÒA, máy chọn ' + randomNumber
-            } else {
-                if (this.choose == 'Kéo') {
-                    if (randomNumber == 'Búa') {
-                        this.result = 'Bạn THUA, máy chọn ' + randomNumber
-                    } else {
-                        this.result = 'Bạn THẮNG, máy chọn ' + randomNumber
-                        this.score++
-                    }
-                } else if (this.choose == 'Búa') {
-                    if (randomNumber == 'Kéo') {
-                        this.result = 'Bạn THẮNG, máy chọn ' + randomNumber
-                        this.score++
-                    } else {
-                        this.result = 'Bạn THUA, máy chọn ' + randomNumber
-                    }
+                if (randomNumber == 0) {
+                    randomNumber = 'Kéo'
+                    this.computer = randomNumber
+                } else if (randomNumber == 1) {
+                    randomNumber = 'Búa'
+                    this.computer = randomNumber
                 } else {
-                    if (randomNumber == 'Búa') {
-                        this.result = 'Bạn THẮNG, máy chọn ' + randomNumber
-                        this.score++
+                    randomNumber = 'Bao'
+                    this.computer = randomNumber
+                }
+
+                if (this.choose == randomNumber) {
+                    this.result = 'Bạn HÒA, máy chọn ' + randomNumber
+                } else {
+                    if (this.choose == 'Kéo') {
+                        if (randomNumber == 'Búa') {
+                            this.result = 'Bạn THUA, máy chọn ' + randomNumber
+                        } else {
+                            this.result = 'Bạn THẮNG, máy chọn ' + randomNumber
+                            this.score++
+                        }
+                    } else if (this.choose == 'Búa') {
+                        if (randomNumber == 'Kéo') {
+                            this.result = 'Bạn THẮNG, máy chọn ' + randomNumber
+                            this.score++
+                        } else {
+                            this.result = 'Bạn THUA, máy chọn ' + randomNumber
+                        }
                     } else {
-                        this.result = 'Bạn THUA, máy chọn ' + randomNumber
+                        if (randomNumber == 'Búa') {
+                            this.result = 'Bạn THẮNG, máy chọn ' + randomNumber
+                            this.score++
+                        } else {
+                            this.result = 'Bạn THUA, máy chọn ' + randomNumber
+                        }
                     }
                 }
             }
-
         },
         resetScore() {
             this.choose = [],
