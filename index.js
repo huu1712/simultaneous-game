81efc10d71d5b6c63eec8de1510217745d96520f
huu1712 <huu10a10vts@gmail.com>
2022-10-12 15:31:53 +0700

    image

--- index.js
+++ index.js
@@ -3,60 +3,70 @@ const {createApp} = Vue
 createApp({
     data() {
         return {
-            choose:[],
-            result:'',
-            b:''
+            choose: [],
+            computer:'',
+            result: '',
+            score: 0,
+            nameCard:'',
         }
     },
-    methods:{
-        clickMe(event, val){
+    methods: {
+        clickMe(event, val) {
+            this.nameCard = val
             this.choose.push(val)
-            if(this.choose.length > 1){
-                this.choose.splice(event,1)
-            }
+            if (this.choose.length > 1) {
+                this.choose.splice(event, 1)
+            }console.log(this.choose)
         },
 
-        randomIndex(){
-            const randomNumber = Math.floor(Math.random() * 3)
-            switch (randomNumber){
-                case 0:
-                    randomNumber == 0;
-                    var a = randomNumber
+        randomIndex() {
+            var randomNumber = Math.floor(Math.random() * 3)
 
-                    if(this.choose == 0){
-                        this.b = 'Huề, máy chọn kéo'
-                    }else if(this.choose == 1){
-                        this.b = 'Bạn thắng, máy chọn kéo'
-                    }else{
-                        this.b = 'Bạn thua, máy chọn kéo'
-                    }
-                    break;
-                case 1:
-                    randomNumber == 1;
-                    var a = randomNumber
-
-                    if(this.choose == 0){
-                        this.b = 'Bạn thua, máy chọn búa'
-                    }else if(this.choose == 1){
-                        this.b = 'Huề, máy chọn búa'
-                    }else{
-                        this.b = 'Bạn thắng, máy chọn búa'
-                    }
-                    break;
-                case 2:
-                    randomNumber == 2;
-                    var a = randomNumber
-
-                    if(this.choose == 0){
-                        this.b = 'Bạn thắng, máy chọn bao'
-                    }else if(this.choose == 1){
-                        this.b = 'Bạn thua, máy chọn bao'
-                    }else{
-                        this.b = 'Huề, máy chọn bao'
-                    }
-                    break;
+            if (randomNumber == 0) {
+                randomNumber = 'Kéo'
+                this.computer = randomNumber
+            } else if (randomNumber == 1) {
+                randomNumber = 'Búa'
+                this.computer = randomNumber
+            } else {
+                randomNumber = 'Bao'
+                this.computer = randomNumber
             }
 
+            if (this.choose == randomNumber) {
+                this.result = 'Bạn HÒA, máy chọn ' + randomNumber
+            } else {
+                if (this.choose == 'Kéo') {
+                    if (randomNumber == 'Búa') {
+                        this.result = 'Bạn THUA, máy chọn ' + randomNumber
+                    } else {
+                        this.result = 'Bạn THẮNG, máy chọn ' + randomNumber
+                        this.score++
+                    }
+                } else if (this.choose == 'Búa') {
+                    if (randomNumber == 'Kéo') {
+                        this.result = 'Bạn THẮNG, máy chọn ' + randomNumber
+                        this.score++
+                    } else {
+                        this.result = 'Bạn THUA, máy chọn ' + randomNumber
+                    }
+                } else {
+                    if (randomNumber == 'Búa') {
+                        this.result = 'Bạn THẮNG, máy chọn ' + randomNumber
+                        this.score++
+                    } else {
+                        this.result = 'Bạn THUA, máy chọn ' + randomNumber
+                    }
+                }
+            }
+
+        },
+        resetScore() {
+            this.choose = [],
+                this.result = '',
+                this.score = 0,
+                this.nameCard = '',
+                this.computer = ''
         }
     }
 }).mount('#app')
